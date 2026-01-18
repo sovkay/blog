994cf2d3f145ec642813c5e02584180b551146ea
Sorv <sorvkay@gmail.com>
2026-01-18 10:56:39 +0530

    fix: update resume

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Sorv';
-export const SITE_DESCRIPTION = 'I build scalable frontends and try to write about things that I find interesting.';
+export const SITE_TITLE = 'Saurav Kumar | Engineer';
+export const SITE_DESCRIPTION = 'I build scalable software products and try to write about things that I find interesting.';
